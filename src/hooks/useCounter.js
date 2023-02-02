9bef4492e4493a21b8bad47160fa290cc85a72d3
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-02-02 18:18:14 +0500

    first commit in custom hooks

---
+++ src/hooks/useCounter.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+const useCounter = () => {
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevState) => prevState + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return counter;
+};
+
+export default useCounter;
